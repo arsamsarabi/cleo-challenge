56269a8fba122dac70bb2f818acab618ad703797
Arsam Sarabi <arsamsarabi@me.com>
2021-03-01 10:49:57 +0000

    🎨 ran linters

--- src/screens/bills/Bills.test.tsx
+++ src/screens/bills/Bills.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import userEvent from '@testing-library/user-event'
 
 import { render, store } from '../../utils'
 import Bills from './Bills'
-import { MockStoreType } from '../../utils'
-import type { Merchant } from '../../store'
-import { patchMerchantBill as mockPatchMerchantBill } from '../../api/merchants'
+import type { Merchant, RootState } from '../../store'
 
 jest.mock('../../api/merchants', () => ({
   patchMerchantBill: jest.fn(() => Promise.resolve()),
@@ -15,7 +12,7 @@ describe('<Bills />', () => {
   test('Should render all merchants', () => {
     const { getByTestId } = render(<Bills />)
 
-    ;(store.getState() as MockStoreType).merchantsStore.merchants.forEach(
+    ;(store.getState() as RootState).merchantsStore.merchants.forEach(
       (merchant: Merchant) => {
         if (merchant.isBill) {
           expect(getByTestId(merchant.id)).toBeDefined()
